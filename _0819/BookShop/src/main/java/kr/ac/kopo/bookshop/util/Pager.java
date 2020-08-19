3589cb5f46b7cafb8cdecc048aa151a6cf334125
ssmapricity <ssmapricity93@gmail.com>
2020-08-19 23:11:59 +0900

    Basic CURD

---
+++ _0819/BookShop/src/main/java/kr/ac/kopo/bookshop/util/Pager.java
@@ -0,0 +1,27 @@
+package kr.ac.kopo.bookshop.util;
+
+public class Pager {
+	int page = 1;
+	int perPage = 10;
+	float total;
+	
+	public int getPage() {
+		return page;
+	}
+	public void setPage(int page) {
+		this.page = page;
+	}
+	public int getPerPage() {
+		return perPage;
+	}
+	public void setPerPage(int perPage) {
+		this.perPage = perPage;
+	}
+	public void setTotal(float total) {
+		this.total = total;
+	}
+	public float getTotal() {
+		return total;
+	}
+	
+}
